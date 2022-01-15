fd67c3f06f68c2bd9da458cb09cc7a61b73e76d2
cadenmr <epintric@gmail.com>
2022-01-15 11:10:44 -0700

    add wheel size to constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,9 @@ package frc.robot;
 public final class Constants {
 
     public static final class DriveBase {
+
+        public static final int wheelSize = 4;  // inches
+
         public static final int leftMotor0 = 0;
         public static final int leftMotor1 = 0;
         public static final int rightMotor0 = 0;
@@ -25,5 +28,4 @@ public final class Constants {
         public static final int rightEncoderA = 2;
         public static final int rightEncoderB = 3;
     }
-
 }
