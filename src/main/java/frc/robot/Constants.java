418a95182517033b8b224558db5f49b69221e1b3
cadenmr <epintric@gmail.com>
2022-01-22 11:19:58 -0700

    update constants once more

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,9 @@ public final class Constants {
 
         public static final double wheelSize = 4.0;  // inches
         public static final double gearboxReductionFactor = 9.47;  // 1 / 9.47
+        public static final int motorTemperatureWarnThreshold = 60;  // 60 deg C
+        public static final int motorCurrentWarnThreshold = 100; //  TODO: Find this
+        public static final int motorCurrentMaxThreshold = 120; //  TODO: Find this
 
         // TODO: find these
         public static final int leftSpark0ID = 0;
