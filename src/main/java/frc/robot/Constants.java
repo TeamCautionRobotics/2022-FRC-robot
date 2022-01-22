ac688863eedd06a98868cf335ba6fee7c2741fbc
cadenmr <epintric@gmail.com>
2022-01-22 09:49:19 -0700

    update constants and add REVLib

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,14 +7,9 @@ public final class Constants {
         public static final int wheelSize = 4;  // inches
 
         // TODO: find these
-        public static final int leftMotor0 = 0;
-        public static final int leftMotor1 = 0;
-        public static final int rightMotor0 = 0;
-        public static final int rightMotor1 = 0;
-        
-        public static final int leftEncoderA = 0;
-        public static final int leftEncoderB = 1;
-        public static final int rightEncoderA = 2;
-        public static final int rightEncoderB = 3;
+        public static final int leftSpark0ID = 0;
+        public static final int leftSpark1ID = 1;
+        public static final int rightSpark0ID = 2;
+        public static final int rightSpark1ID = 3;
     }
 }
