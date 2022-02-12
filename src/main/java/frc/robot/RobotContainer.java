c31860af6e718b07dfbd537514d59da44955ba44
cadenmr <epintric@gmail.com>
2022-02-11 22:33:21 -0700

    import lib, add subsystem to container, auto-retract

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,13 +1,18 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.misc2022.EnhancedJoystick;
 import frc.misc2022.Gamepad;
 import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Intake;
 
 
 public class RobotContainer {
@@ -17,16 +22,23 @@ public class RobotContainer {
   public final Gamepad manipulator = new Gamepad(Constants.Driver.manipulatorPort);
 
   public final DriveBase driveBase;
+  public final Intake intake;
 
   public final CANSparkMax leftDrive0 = new CANSparkMax(Constants.DriveBase.leftSpark0ID, MotorType.kBrushless);
   public final CANSparkMax leftDrive1 = new CANSparkMax(Constants.DriveBase.leftSpark1ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive0 = new CANSparkMax(Constants.DriveBase.rightSpark0ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive1 = new CANSparkMax(Constants.DriveBase.rightSpark1ID, MotorType.kBrushless);
 
+  public final WPI_VictorSPX intakeMotor = new WPI_VictorSPX(Constants.Intake.intakeMotorID);
+  public final Solenoid intakeDeploy = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.intakePistonID);
+
   public RobotContainer() {
     configureButtonBindings();
 
     driveBase = new DriveBase(leftDrive0, leftDrive1, rightDrive0, rightDrive1);
+    intake = new Intake(intakeMotor, intakeDeploy);
+
+
     // getDistance returns inches, getRate returns inches/second
     driveBase.setDistancePerPulse((1.0 / Constants.DriveBase.gearboxReductionFactor) * Constants.DriveBase.wheelSize * Math.PI);
 
