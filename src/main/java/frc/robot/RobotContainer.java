beb226f92ea513c397339c29cd952a3a9ed7a0d6
cadenmr <epintric@gmail.com>
2022-02-28 19:59:52 -0700

    add conveyor to robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,12 +2,18 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.misc2022.EnhancedJoystick;
 import frc.misc2022.Gamepad;
+import frc.robot.Constants;
 import frc.robot.commands.TankDrive;
+import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.DriveBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 
 public class RobotContainer {
@@ -17,11 +23,16 @@ public class RobotContainer {
   public final Gamepad manipulator = new Gamepad(Constants.Driver.manipulatorPort);
 
   public final DriveBase driveBase;
+  public final Conveyor conveyor;
 
   public final CANSparkMax leftDrive0 = new CANSparkMax(Constants.DriveBase.leftSpark0ID, MotorType.kBrushless);
   public final CANSparkMax leftDrive1 = new CANSparkMax(Constants.DriveBase.leftSpark1ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive0 = new CANSparkMax(Constants.DriveBase.rightSpark0ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive1 = new CANSparkMax(Constants.DriveBase.rightSpark1ID, MotorType.kBrushless);
+
+  public final Solenoid gatePiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Conveyor.gatePCMChannel);
+  public final WPI_VictorSPX conveyorMotor = new WPI_VictorSPX(Constants.Conveyor.motorID);
+  
 
   public RobotContainer() {
     configureButtonBindings();
@@ -31,6 +42,8 @@ public class RobotContainer {
     driveBase.setDistancePerPulse((1.0 / Constants.DriveBase.gearboxReductionFactor) * Constants.DriveBase.wheelSize * Math.PI);
 
     driveBase.setDefaultCommand(new TankDrive(driveBase, () -> leftJoystick.getY(), () -> rightJoystick.getY()));
+
+    conveyor = new Conveyor(conveyorMotor, gatePiston);
 
   }
 
