e383c793d90dffd3b426341c475970ff090e6a58
cadenmr <caden.m.r@cox.net>
2022-03-11 12:46:47 -0700

    remove limiter cuz dumb

--- src/main/java/frc/robot/subsystems/ClimbAngle.java
+++ src/main/java/frc/robot/subsystems/ClimbAngle.java
@@ -73,13 +73,16 @@ public class ClimbAngle extends SubsystemBase {
    */
   public void setPosition(double reference) {
 
-    if (reference > 110) {
-      setpoint = 0;
-    } else if (reference < 0) {
-      setpoint = 0;
-    } else {
-      setpoint = reference;
-    }
+    setpoint = reference;
+
+    // if (reference > 110) {
+    //   setpoint = 0;
+    // } else if (reference < 0) {
+    //   setpoint = 0;
+    // } else {
+    //   setpoint = reference;
+    // }
+
   }
 
   /**
