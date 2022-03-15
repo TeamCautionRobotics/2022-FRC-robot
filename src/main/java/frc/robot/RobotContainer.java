56b0eaa6bdc65e45b3811a0c6cf52084c6d135a3
cadenmr <caden.m.r@cox.net>
2022-03-14 21:33:13 -0700

    cleanup robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.misc2022.EnhancedJoystick;
-import frc.misc2022.Gamepad;
 import frc.robot.commands.AutoAtGoal;
 import frc.robot.commands.AutoGrabShootBall;
 import frc.robot.commands.Auto_DriveThreeFeet;
@@ -34,77 +33,66 @@ public class RobotContainer {
 
   public final EnhancedJoystick leftJoystick = new EnhancedJoystick(Constants.Driver.leftJoystickPort);
   public final EnhancedJoystick rightJoystick = new EnhancedJoystick(Constants.Driver.rightJoystickPort);
-  public final Gamepad manipulator = new Gamepad(Constants.Driver.manipulatorPort);
 
   public final JoystickButton shootBallButton = new JoystickButton(leftJoystick, 1);
   public final JoystickButton grabBallButton = new JoystickButton(leftJoystick, 3);
   public final JoystickButton ejectBallButton = new JoystickButton(leftJoystick, 5);
-  // public final JoystickButton intakeDeployButton = new JoystickButton(leftJoystick, 6);
-  // public final JoystickButton intakeMotorButton = new JoystickButton(leftJoystick, 7);  
-  // public final JoystickButton conveyorGateButton = new JoystickButton(rightJoystick, 11);
-  // public final JoystickButton conveyorMotorButton = new JoystickButton(rightJoystick, 10);
   
   public final CANSparkMax leftDrive0 = new CANSparkMax(Constants.DriveBase.leftSpark0ID, MotorType.kBrushless);
   public final CANSparkMax leftDrive1 = new CANSparkMax(Constants.DriveBase.leftSpark1ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive0 = new CANSparkMax(Constants.DriveBase.rightSpark0ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive1 = new CANSparkMax(Constants.DriveBase.rightSpark1ID, MotorType.kBrushless);
   
-  public final Solenoid gatePiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Conveyor.gatePCMChannel);
+  public final Solenoid conveyorPiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Conveyor.gatePCMChannel);
   public final WPI_VictorSPX conveyorMotor = new WPI_VictorSPX(Constants.Conveyor.motorID);
 
+  public final Solenoid intakePiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Intake.pistonPCMChannel);
   public final WPI_VictorSPX intakeMotor = new WPI_VictorSPX(Constants.Intake.intakeMotorID);
-  public final Solenoid intakeDeploy = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Intake.pistonPCMChannel);
+
+  public final UsbCamera cam0 = CameraServer.startAutomaticCapture("Rear Camera", 0);
+  public final UsbCamera cam1 = CameraServer.startAutomaticCapture("Front Camera", 1);
 
   public final DriveBase driveBase = new DriveBase(leftDrive0, leftDrive1, rightDrive0, rightDrive1);
-  public final Conveyor conveyor = new Conveyor(conveyorMotor, gatePiston);
-  public final Intake intake = new Intake(intakeMotor, intakeDeploy);
+  public final Conveyor conveyor = new Conveyor(conveyorMotor, conveyorPiston);
+  public final Intake intake = new Intake(intakeMotor, intakePiston);
 
   public final SequentialCommandGroup liftIntakeDelayed = new SequentialCommandGroup(new WaitCommand(1), new SetIntakeDeploy(intake, false));
   final SendableChooser<Command> autonomousChooser = new SendableChooser<>();
 
-  public UsbCamera cam0 = CameraServer.startAutomaticCapture("Rear Camera", 0);
-  public UsbCamera cam1 = CameraServer.startAutomaticCapture("Front Camera", 1);
-
   public RobotContainer() {
-    configureButtonBindings();
 
+    // buttons
+    configureButtonBindings();
+    
+    // configure devices
+    driveBase.setDistancePerPulse((1.0 / Constants.DriveBase.gearboxReductionFactor) * Constants.DriveBase.wheelSize * Math.PI);  // inches, inches/sec
+    conveyorMotor.setNeutralMode(NeutralMode.Brake);
+    intakeMotor.setInverted(true);
+    intakeMotor.setNeutralMode(NeutralMode.Brake);
     cam0.setFPS(15);
     cam1.setFPS(15);
-    
-    // config
-    conveyorMotor.setNeutralMode(NeutralMode.Brake);  // brake the conveyor when stopped
-
-    intakeMotor.setInverted(true);  // invert the intake motor
-    intakeMotor.setNeutralMode(NeutralMode.Brake);  // brake the intake when stopped
-
-    driveBase.setDistancePerPulse((1.0 / Constants.DriveBase.gearboxReductionFactor) * Constants.DriveBase.wheelSize * Math.PI);  // getDistance returns inches, getRate returns inches/second
     
     // default commands
     driveBase.setDefaultCommand(new TankDrive(driveBase, () -> leftJoystick.getY(), () -> rightJoystick.getY()));
 
+    // set up autonomous chooser
     autonomousChooser.setDefaultOption("Do Nothing Autonomous", new InstantCommand());
     autonomousChooser.addOption("Drive forward", new Auto_DriveThreeFeet(driveBase));
     autonomousChooser.addOption("Grab ball and shoot", new AutoGrabShootBall(driveBase, conveyor, intake));
-    // autonomousChooser.addOption("Grab ball and shoot two: The sequel", new AutoGrabShootBall2(driveBase, conveyor, intake));
     autonomousChooser.addOption("At goal: shoot, grab, shoot", new AutoAtGoal(driveBase, conveyor, intake));
     SmartDashboard.putData(autonomousChooser);
   }
 
   private void configureButtonBindings() {
 
-    // conveyorGateButton.whenPressed(new ToggleConveyorGate(conveyor));
-    // conveyorMotorButton.whenHeld(new RunConveyorMotor(conveyor));
-    
-    // intakeDeployButton.whenPressed(new ToggleIntakeDeploy(intake));
-    // intakeMotorButton.whileHeld(new RunIntakeMotor(intake));
-
     grabBallButton.whileHeld(new GrabBall(intake, conveyor));
     grabBallButton.whenReleased(liftIntakeDelayed);
-    shootBallButton.whileHeld(new ShootBall_HighPower(conveyor));
+
     ejectBallButton.whenHeld(new EjectBall(intake, conveyor));
 
-  }
+    shootBallButton.whileHeld(new ShootBall_HighPower(conveyor));
 
+  }
 
   public Command getAutonomousCommand() {
     return autonomousChooser.getSelected();
