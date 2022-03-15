4b1cad6c0d3ee432ae618ded4b733a7816a91fe5
cadenmr <caden.m.r@cox.net>
2022-03-14 21:22:02 -0700

    bugfix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,10 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.AutoAtGoal;
+import frc.robot.commands.AutoGrabShootBall;
+import frc.robot.commands.Auto_DriveThreeFeet;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -73,6 +77,13 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+
+    // fix bug that causes a crash when auto is enabled more than once
+    m_robotContainer.autonomousChooser.setDefaultOption("Do Nothing Autonomous", new InstantCommand());
+    m_robotContainer.autonomousChooser.addOption("Drive forward", new Auto_DriveThreeFeet(m_robotContainer.driveBase));
+    m_robotContainer.autonomousChooser.addOption("Grab ball and shoot", new AutoGrabShootBall(m_robotContainer.driveBase, m_robotContainer.conveyor, m_robotContainer.intake));
+    m_robotContainer.autonomousChooser.addOption("At goal: shoot, grab, shoot", new AutoAtGoal(m_robotContainer.driveBase, m_robotContainer.conveyor, m_robotContainer.intake));
+
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
