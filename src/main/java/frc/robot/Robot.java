170baea3fab33f17e324aee5977b938bcefd093a
cadenmr <epintric@gmail.com>
2022-03-15 19:33:10 -0700

    remove unused import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.commands.AutoAtGoal;
-import frc.robot.commands.AutoGrabShootBall;
-import frc.robot.commands.Auto_DriveThreeFeet;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
