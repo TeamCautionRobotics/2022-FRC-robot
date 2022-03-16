64bfba55e59d2646c362913a5c4f7e192fab1bb0
cadenmr <caden.m.r@cox.net>
2022-03-15 20:26:52 -0700

    standardize and clean up

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,6 @@ public final class Constants {
 
         public static final int leftJoystickPort = 0;
         public static final int rightJoystickPort = 1;
-        public static final int manipulatorPort = 2;
         
     }
 
@@ -18,13 +17,11 @@ public final class Constants {
 
     public static final class DriveBase {
 
-        public static final double wheelSize = 4.0;  // inches
-        public static final double gearboxReductionFactor = 9.47;  // 1 / 9.47
-        public static final int motorTemperatureWarnThreshold = 60;  // 60 deg C
-        public static final int motorCurrentWarnThreshold = 100; //  TODO: Find this
-        public static final int motorCurrentMaxThreshold = 120; //  TODO: Find this
+        public static final double encoderConversionFactor = (1.0 / 9.47) * 4.0 * Math.PI;
+        public static final int motorTemperatureWarnThreshold = 60;
+        public static final int motorCurrentWarnThreshold = 100;
+        public static final int motorCurrentMaxThreshold = 120; 
 
-        // TODO: find these
         public static final int leftSpark0ID = 10;
         public static final int leftSpark1ID = 11;
         public static final int rightSpark0ID = 12;
@@ -34,12 +31,11 @@ public final class Constants {
     public static final class Conveyor {
 
         public static final int motorID = 21;
-        public static final int gatePCMChannel = 02;
+        public static final int pistonPCMChannel = 02;
     }
         
     public static final class Intake {
 
-        // TODO: Find these
         public static final int pistonPCMChannel = 01;
         public static final int intakeMotorID = 20;
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,7 +43,7 @@ public class RobotContainer {
   public final CANSparkMax rightDrive0 = new CANSparkMax(Constants.DriveBase.rightSpark0ID, MotorType.kBrushless);
   public final CANSparkMax rightDrive1 = new CANSparkMax(Constants.DriveBase.rightSpark1ID, MotorType.kBrushless);
   
-  public final Solenoid conveyorPiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Conveyor.gatePCMChannel);
+  public final Solenoid conveyorPiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Conveyor.pistonPCMChannel);
   public final WPI_VictorSPX conveyorMotor = new WPI_VictorSPX(Constants.Conveyor.motorID);
 
   public final Solenoid intakePiston = new Solenoid(Constants.Misc.pcmID, PneumaticsModuleType.CTREPCM, Constants.Intake.pistonPCMChannel);
@@ -67,7 +67,7 @@ public class RobotContainer {
     // configure devices
     leftDrive0.setInverted(true);
     leftDrive1.setInverted(true);
-    driveBase.setDistancePerPulse((1.0 / Constants.DriveBase.gearboxReductionFactor) * Constants.DriveBase.wheelSize * Math.PI);  // inches, inches/sec
+    driveBase.setEncoderConversionFactor(Constants.DriveBase.encoderConversionFactor);  // inches, inches/sec
     conveyorMotor.setNeutralMode(NeutralMode.Brake);
     intakeMotor.setInverted(true);
     intakeMotor.setNeutralMode(NeutralMode.Brake);
